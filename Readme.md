fcb35817a62a476d9665c09ac0e3a198056f682a
Dhanaraj <dhanarajbhaskar690@gmail.com>
2023-01-07 23:45:10 +0530

    template String

--- Readme.md
+++ Readme.md
@@ -1,3 +1,4 @@
 1. Var let Const
 2. Arraow function
 3. Default Parameter
+4. Template String
---
+++ TemplateString.js
@@ -0,0 +1,10 @@
+// Template literals (Template strings)
+
+// Template literals are literals delimited with backtick (`) characters, allowing for multi-line strings, string interpolation with embedded expressions,
+const a = 5;
+const b = 10;
+console.log(`Fifteen is ${a + b} and 
+not 
+${2 * a + b}.`);
+// "Fifteen is 15 and
+// not 20."
