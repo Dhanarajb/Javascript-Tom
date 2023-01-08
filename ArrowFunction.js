2151631234b2d977229106122724713b5e0c934d
Dhanaraj <dhanarajbhaskar690@gmail.com>
2023-01-08 11:25:33 +0530

    arrow

--- ArrowFunction.js
+++ ArrowFunction.js
@@ -4,15 +4,20 @@
 // Arrow functions don't have their own bindings to this, arguments, or super, and should not be used as methods.
 // Arrow functions cannot be used as constructors. Calling them with new throws a TypeError. They also don't have access to the new.target keyword.
 // Arrow functions cannot use yield within their body and cannot be created as generator functions.
+let data = [10, 20, 30, 40, 50];
+
+let newData = data.map((item) => item * 2);
+console.log(newData);
 
 const student = {
   marks: 65,
   showMarks() {
-    setTimeout(function () {
-      console.log("settimeout: ", this);
-    });
     console.log("normal ", this);
   },
+  getMarks: () => console.log(this),
 };
 
 student.showMarks();
+student.getMarks();
+
+// we cant bind this because it will redirect to window objwct
