d99067c193aeef8bfcb152e19a38ceb67c148636
Dhanaraj <dhanarajbhaskar690@gmail.com>
2023-01-09 12:40:40 +0530

    callback

---
+++ Callback.js
@@ -0,0 +1,12 @@
+// // callback:any function that is passed as an argument called callback function
+// // or
+// // a callback is a function that is to be executed after another function has finished executing
+
+// function pass(){
+//     console.log("hello")
+// }
+// function add(a,b,callback){
+//     console.log(a+b);
+//     callback()
+// }
+// add(10,12)
