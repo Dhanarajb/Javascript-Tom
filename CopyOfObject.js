75996580ff4e8ec4887c089eb1e44a5909ddb5c0
Dhanaraj <88299676+Dhanarajb@users.noreply.github.com>
2023-02-02 00:23:00 +0530

    Update CopyOfObject.js

--- CopyOfObject.js
+++ CopyOfObject.js
@@ -1,29 +1,29 @@
 // Shallow copy: if two properties of object reference is same
 
-// let dev1 = { name: "Tom" };
-// let dev2 = dev1;
-// dev2.name = "sweet";
+let dev1 = { name: "Tom" };
+let dev2 = dev1;
+dev2.name = "sweet";
 
-// console.log(dev1);
-// console.log(dev2);
+console.log(dev1);
+console.log(dev2);
 
 // Deep coly:If two properties object references is different
 
-// let dev1 = { name: "Tom" };
-// // let dev2 = Object.assign({}, dev1);
-// let dev2 = { ...dev1 };
-// dev2.name = "sweet";
+let dev1 = { name: "Tom" };
+let dev2 = Object.assign({}, dev1);
+let dev2 = { ...dev1 };
+dev2.name = "sweet";
 
-// console.log(dev1);
-// console.log(dev2);
+console.log(dev1);
+console.log(dev2);
 
-// let dev1 = { name: "Tom", skills: { primary: "full", secondary: "ux" } };
-// let dev2 = JSON.parse(JSON.stringify(dev1));
-// dev2.name = "sweet";
-// dev2.skills.primary = "front end";
+let dev1 = { name: "Tom", skills: { primary: "full", secondary: "ux" } };
+let dev2 = JSON.parse(JSON.stringify(dev1));
+dev2.name = "sweet";
+dev2.skills.primary = "front end";
 
-// console.log(dev1);
-// console.log(dev2);
+console.log(dev1);
+console.log(dev2);
 
 // ---------------------lodash-----------------
 const _ = require("lodash");
